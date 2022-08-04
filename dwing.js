c149137bea3ca0d1588fd4cf90001afa7121c58d
decrovert <decrovertp@gmail.com>
2022-08-04 14:17:57 +0100

    slowing down the code

--- dwing.js
+++ dwing.js
@@ -1,4 +1,7 @@
-var x11 = require( 'x11' );
+import x11 from "x11";
+import { slugdown } from "slugdown";
+
+slugdown.setSlugdownTime(250);
 
 /* Constants */
 var MOD_1_MASK = 1 << 3,
@@ -19,22 +22,31 @@ x11.createClient( function( error, display ) {
     X.GrabButton( display.screen[0].root, true, x11.eventMask.ButtonPress | x11.eventMask.ButtonRelease | x11.eventMask.PointerMotion,
         GRAB_MODE_ASYNC, GRAB_MODE_ASYNC, NONE, NONE, 3, MOD_1_MASK );
 } ).on( 'event', function( event ) {
+    slugdown.slugdown(() => {});
+
     if( event.name === 'KeyPress' && event.child !== 0 ) {
-        X.RaiseWindow( event.child );
+        slugdown.slugdown(() => {
+            X.RaiseWindow( event.child );
+        });
     } else if( event.name === 'ButtonPress' && event.child !== 0 ) {
-        X.GetGeometry( event.child, function( error, attributes ) {
-            start = event;
-            attr = attributes;
-        } );
+        slugdown.slugdown(() => {
+            X.GetGeometry( event.child, function( error, attributes ) {
+                start = event;
+                attr = attributes;
+            } );
+        });
     } else if( event.name === 'MotionNotify' && typeof start !== 'undefined' && start.child !== 0 ) {
         var xdiff = event.rootx - start.rootx,
             ydiff = event.rooty - start.rooty;
-        X.MoveResizeWindow( start.child,
-            attr.xPos + ( start.keycode === 1 ? xdiff : 0 ),
-            attr.yPos + ( start.keycode === 1 ? ydiff : 0 ),
-            Math.max( 1, attr.width + ( start.keycode === 3 ? xdiff : 0 ) ),
-            Math.max( 1, attr.height + ( start.keycode === 3 ? ydiff : 0 ) ) );
+
+        slugdown.slugdown(() => {
+            X.MoveResizeWindow( start.child,
+                attr.xPos + ( start.keycode === 1 ? xdiff : 0 ),
+                attr.yPos + ( start.keycode === 1 ? ydiff : 0 ),
+                Math.max( 1, attr.width + ( start.keycode === 3 ? xdiff : 0 ) ),
+                Math.max( 1, attr.height + ( start.keycode === 3 ? ydiff : 0 ) ) );
+       });
     } else if( event.name === 'ButtonRelease' ) {
-        start = undefined;
+        slugdown.slugdown(() => { start = undefined; });
     }
 } );
